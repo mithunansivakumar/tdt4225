bd76b4ed9ebceaee96ab78d84a7a4e402a33bee0
mithunansivakumar <mithunan2522@gmail.com>
2023-09-23 12:47:20 +0200

    Added insert user data query

--- task1.py
+++ task1.py
@@ -1,13 +1,22 @@
 from DbConnector import DbConnector
 from tabulate import tabulate
-
+import logging
+import os
 
 class Task1:
 
     def __init__(self):
+        print("hei")
         self.connection = DbConnector()
         self.db_connection = self.connection.db_connection
-        self.cursor = self.connection.cursor
+        self.cursor = self.connection.cursor        
+
+    def get_labels(self):
+        f = open("data/dataset/labeled_ids.txt")
+        lines = f.read().splitlines()
+        f.close()
+        return lines
+
 
     def create_user_table(self, table_name):
         query = """CREATE TABLE IF NOT EXISTS %s (
@@ -17,6 +26,29 @@ class Task1:
         # This adds table_name to the %s variable and executes the query
         self.cursor.execute(query % table_name)
         self.db_connection.commit()
+        logging.info("created user table")
+
+    
+    def insert_user_data(self, table_name):
+        labels = self.get_labels()
+        print(labels)
+        user_ids = os.walk("data/dataset/Data")
+        for i in user_ids:
+            user_ids = i[1]
+            break
+
+        query = """INSERT INTO %s (id, has_labels) VALUES (%s, %s)"""
+
+        for i in user_ids:
+            has_labels = True if i in labels else False
+            self.cursor.execute(query % (table_name, i, has_labels))
+
+        self.db_connection.commit()
+
+        
+
+
+        logging.info("inserted user data")
 
     def create_acitivity_table(self, table_name):
         query = """CREATE TABLE IF NOT EXISTS %s (
@@ -47,6 +79,10 @@ class Task1:
         # This adds table_name to the %s variable and executes the query
         self.cursor.execute(query % table_name)
         self.db_connection.commit()
+    
+    def insert_user_data(self):
+        print("hei")
+
 
 def main():
     program = None
@@ -67,3 +103,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
